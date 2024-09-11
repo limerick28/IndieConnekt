1231bcdc357bb161aaadd3080681fd635bff6864
limerick28 <ericklima2805@gmail.com>
2024-09-11 11:13:55 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -4,32 +4,20 @@ $username = $_POST['username'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 $conexao = mysqli_connect('localhost', 'root', '', 'indieconnekt');
+
 if (!$conexao) {
-    die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
+    die("Falha na conexão: " . mysqli_connect_error());
 }
-    $sql = "INSERT INTO usuarios (username, email, senha) VALUES ('$username', '$email', '$senha')";
-    $executar = mysqli_query($conexao, $sql);
+$senha_hash = password_hash($senha, PASSWORD_DEFAULT);
+$sql = "INSERT INTO usuarios (username, email, senha) VALUES ('$username', '$email', '$senha_hash')";
 
+$exe = mysqli_query($conexao, $sql);
 
-    if ($executar) {
-        echo "Cadastro realizado com sucesso! <a href='LOGIN.html'>Fazer login</a>";
-    } else {
-        echo "Erro ao cadastrar! <a href='LOGIN.html'>Tentar Novamente</a>";
-    }
-
-    $res = mysqli_fetch_array($executar);
-  
-    if ( $res['senha']) {
-        $_SESSION['nome'] = $res['username'];
-        $_SESSION['id'] = $res['id'];
-        $_SESSION['adm'] = $res['adm'];
-        header('Location: inicial.php');
-        exit;
-    } else {
-        echo "Login e/ou senha incorretos! <a href='login.html'>Tentar Novamente</a>";
-    }
-
+if ($exe) {
+    echo "Cadastro realizado com sucesso!";
+} else {
+    echo "Erro ao cadastrar: " . mysqli_error($conexao);
+}
 
 mysqli_close($conexao);
-include('pagina.html');
 ?>
