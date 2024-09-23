223b637995e07ea7c50d34ae716dbdf35f033b56
limerick28 <ericklima2805@gmail.com>
2024-09-23 07:43:44 -0300

    Update and rename LOGIN.php to verificar.php

---
+++ verificar.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$senha = $_POST['senha'];
+$login = $_POST['email'];
+$conexao = mysqli_connect('localhost','root','','indieconnekt');
+$sql = "SELECT * FROM usuarios WHERE email like '$login'
+and senha like '$senha' ";
+$executar = mysqli_query($conexao, $sql);
+$res = mysqli_fetch_array($executar);
+if($res['email'] != NULL){
+   $_SESSION['email'] = $res['email'];
+   header("Location: pagina.php");
+}
+else{
+   echo "Login e/ou senha incorretos";
+}
+$fechar = mysqli_close($conexao);
+?>
