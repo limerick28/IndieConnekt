52fe3805b272bc3892c463c4a407ae7a339c047e
limerick28 <ericklima2805@gmail.com>
2024-10-01 08:56:44 -0300

    Update sessao.php

--- sessao.php
+++ sessao.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$username = $_SESSION['username'];
 $email = $_SESSION['email'];
+$id = $_SESSION['id'];
 if($email == NULL){
 	die("Usuário não autenticado!
 	<a href='LOGIN.php'>Logar</a>");
