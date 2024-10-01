f77a488018041b448c7208ed89d33b4f49a18d20
limerick28 <ericklima2805@gmail.com>
2024-10-01 08:55:57 -0300

    Update verificar.php

--- verificar.php
+++ verificar.php
@@ -4,11 +4,12 @@ $senha = $_POST['senha'];
 $login = $_POST['email'];
 $conexao = mysqli_connect('localhost','root','','indieconnekt');
 $sql = "SELECT * FROM usuarios WHERE email like '$login'
-and senha like '$senha' ";
+and senha like '$senha'";
 $executar = mysqli_query($conexao, $sql);
 $res = mysqli_fetch_array($executar);
 if($res['email'] != NULL){
    $_SESSION['email'] = $res['email'];
+   $_SESSION['id'] = $res['idusuarios'];
    header("Location: pagina.php");
 }
 else{
