ea5ec35a0f931a50bb0a7d5174ba74dfc53a6ac0
limerick28 <ericklima2805@gmail.com>
2024-10-01 07:43:01 -0300

    Update indieconnekt.sql

--- indieconnekt.sql
+++ indieconnekt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 10-Set-2024 às 14:10
+-- Generation Time: 01-Out-2024 às 12:42
 -- Versão do servidor: 10.1.38-MariaDB
 -- versão do PHP: 5.6.40
 
@@ -34,6 +34,13 @@ CREATE TABLE `genero` (
   `descricao` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `genero`
+--
+
+INSERT INTO `genero` (`idGenero`, `nomeGenero`, `descricao`) VALUES
+(1, 'aventura', 'navegue por mundos extraordinarios');
+
 -- --------------------------------------------------------
 
 --
@@ -41,11 +48,56 @@ CREATE TABLE `genero` (
 --
 
 CREATE TABLE `jogos` (
-  `id` mediumint(5) NOT NULL,
+  `idjogos` mediumint(5) NOT NULL,
   `nome` varchar(25) CHARACTER SET utf8mb4 NOT NULL,
-  `genero` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `idGenero` varchar(20) CHARACTER SET utf8 NOT NULL,
   `foto` varchar(50) CHARACTER SET utf8mb4 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `jogos`
+--
+
+INSERT INTO `jogos` (`idjogos`, `nome`, `idGenero`, `foto`) VALUES
+(1, 'the dark legends of the c', '1', 'darklegendsed.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `listaDesejo`
+--
+
+CREATE TABLE `listaDesejo` (
+  `idlista` int(6) NOT NULL,
+  `idusuarios` int(6) NOT NULL,
+  `idjogos` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `posts`
+--
+
+CREATE TABLE `posts` (
+  `nomePost` varchar(60) CHARACTER SET utf8 NOT NULL,
+  `id_jogos` int(8) NOT NULL,
+  `comentario` text CHARACTER SET utf8,
+  `foto` varchar(50) DEFAULT NULL,
+  `id_idusuarios` int(8) NOT NULL,
+  `idPost` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `posts`
+--
+
+INSERT INTO `posts` (`nomePost`, `id_jogos`, `comentario`, `foto`, `id_idusuarios`, `idPost`) VALUES
+('jogo 1', 0, 'pipipipipipipipopopopopopopop', 'pixilart-drawing.png', 0, 1),
+('jogo 2', 0, 'pipipipipipipipipipipipip', 'hollowknighted.jpg', 0, 7),
+('jogo 3', 0, 'gigigiigigigigi', 'conscripted.jpg', 0, 10),
+('jogo 4', 0, 'jogo perfeito', 'darklegendsed.jpg', 0, 11),
+('jogo 5', 0, 'jogo merda', 'f1fa58f80ab1e08981a6a1e436de39a10e4bcc245785b303af', 0, 14);
 
 -- --------------------------------------------------------
 
@@ -54,13 +106,27 @@ CREATE TABLE `jogos` (
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(8) NOT NULL,
+  `idusuarios` int(8) NOT NULL,
   `username` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
   `senha` varchar(20) NOT NULL,
-  `fotouser` varchar(50) CHARACTER SET utf8mb4 NOT NULL,
-  `adm` tinyint(1) NOT NULL DEFAULT '0'
+  `fotouser` longblob,
+  `comentarios` int(6) DEFAULT NULL,
+  `avaliacao` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`idusuarios`, `username`, `email`, `senha`, `fotouser`, `comentarios`, `avaliacao`) VALUES
+(12345678, 'rafael', 'rafael@gmail.com', 'rafael1234', '', NULL, NULL),
+(12345682, 'erick', 'erick@gmail.com', 'erick1234', '', NULL, NULL),
+(12345696, 'edineia', 'edineia@gmail.com', 'edineiaprof', '', NULL, NULL),
+(12345697, 'pedro', 'pedro@gmail.com', 'pedro1234', NULL, NULL, NULL),
+(12345698, 'gemeos', 'gemeos@gmail.com', 'gemeos1234', NULL, NULL, NULL),
+(12345699, 'viadogames', 'viadogames@gmail.com', 'viadogames', NULL, NULL, NULL),
+(12345700, 'fulano', 'fulano@gmail.com', '1234', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -76,14 +142,26 @@ ALTER TABLE `genero`
 -- Indexes for table `jogos`
 --
 ALTER TABLE `jogos`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`idjogos`),
   ADD UNIQUE KEY `nome` (`nome`);
+
+--
+-- Indexes for table `listaDesejo`
+--
+ALTER TABLE `listaDesejo`
+  ADD PRIMARY KEY (`idlista`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`idPost`);
 
 --
 -- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`idusuarios`),
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `username` (`username`);
 
@@ -95,19 +173,31 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `genero`
 --
 ALTER TABLE `genero`
-  MODIFY `idGenero` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `idGenero` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `jogos`
 --
 ALTER TABLE `jogos`
-  MODIFY `id` mediumint(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `idjogos` mediumint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `listaDesejo`
+--
+ALTER TABLE `listaDesejo`
+  MODIFY `idlista` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `idPost` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
+  MODIFY `idusuarios` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12345701;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
