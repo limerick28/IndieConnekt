d7c1d6098bf11e6c540caaa3f08959874cf5165a
limerick28 <ericklima2805@gmail.com>
2024-10-01 08:57:17 -0300

    Update cadProd.php

--- cadProd.php
+++ cadProd.php
@@ -2,17 +2,17 @@
 include("sessao.php");
 $nome = $_POST['nomePost'];
 $comentario = $_POST['comentario'];
-$uploaddir = 'c:/xampp/htdocs/INDIECONNEKT/imagens/';
-$nomeArq = basename($_FILES['arquivo']['name']);
+$uploaddir = 'images/';
+$nomeArq = basename($_FILES['foto']['name']);
 $uploadfile = $uploaddir.$nomeArq;
 $conexao = mysqli_connect
 ('localhost','root','','indieconnekt');
-$sql = "INSERT into posts (nomePost, comentario, foto)
-values ('$nome', '$comentario', '$nomeArq')";
+$sql = "INSERT into posts (nomePost, comentario, foto, id_idusuarios)
+values ('$nome', '$comentario', '$nomeArq', $id)";
 echo $sql;
 $executar = mysqli_query($conexao, $sql);
 
-if (move_uploaded_file($_FILES['arquivo']['tmp_name'], $uploadfile)) {
+if (move_uploaded_file($_FILES['foto']['tmp_name'], $uploadfile)) {
 	if($executar){
 		echo "cadastrado";
 	}
