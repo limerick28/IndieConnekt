f968f69ff48ce749c5efaefdc76e88732ddd7a9d
rafaelfag4 <rafael.eugeniof@gmail.com>
2024-10-11 08:21:22 -0300

    Update perfil.php

--- perfil.php
+++ perfil.php
@@ -58,48 +58,114 @@ session_start();
             <div class="sidebar">
                 <div class="comments" style="background-color: #2e2e4e; padding: 20px; border-radius: 10px; margin-top: 20px;">
                     <h2 style="color: #ffffff;">Comentários</h2>
-                    <p style="color: #aaaaaa;">Você não possui comentários ainda.</p>
+<?php
+if (!isset($_SESSION['id'])) {
+    echo "Você precisa estar logado para ver os posts.";
+    exit;
+}
+
+$idUsuarioLogado = $_SESSION['id'];
+$con = mysqli_connect('localhost', 'root', '', 'indieconnekt');
+
+$sql2 = "SELECT * FROM comentarios ORDER BY idcomentario DESC";
+$exe2 = mysqli_query($con, $sql2);
+
+if (mysqli_num_rows($exe2) > 0) {
+    echo "<table style=' 
+    width: 250px;
+    background-color: #2e2e4e;
+    padding: 20px;
+    border-radius: 10px;
+
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+
+    text-align: center;
+
+    margin-bottom: 20px;
+
+    width: 100%;
+    padding: 5px;
+    border: none;
+    border-radius: 5px;
+    margin-bottom: 10px;
+
+    width: 100%;
+    padding: 5px;
+    border: none;
+    border-radius: 5px;
+    background-color: #333;
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+
+    margin-bottom: 20px;
+
+    width: 100%;
+    padding: 5px;
+    border: none;
+    border-radius: 5px;
+    margin-bottom: 10px;
+
+    background-color: #333;
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 10px;'>";
+
+    while ($res2 = mysqli_fetch_array($exe2)) {
+        $texto = htmlspecialchars($res2['texto']);
+        $fotocomentario = htmlspecialchars($res2['fotocomentario']);
+
+        echo "<tr>
+                <td style='background-color: #2e2e4e; padding: 20px; border-radius: 10px; margin-top: 20px;'>Comentário: $texto</td>
+                <td style='padding: 10px;'>Data: $fotocomentario</td>
+            </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Nenhum comentário encontrado.";
+}
+// Verifica a conexão
+if (!$con) {
+    die("Conexão falhou: " . mysqli_connect_error());
+}
+?>
+
                     <a href="comentando.php" style="color: #91ff10; text-decoration: underline;">Clique aqui para adicionar um comentário</a>
                 </div>
             </div>
 
             <div class="content">
-                <?php
-                if (!isset($_SESSION['id'])) {
-                    echo "Você precisa estar logado para ver os posts.";
-                    exit;
-                }
+            
+<?php
+$sql1 = "SELECT * FROM posts WHERE id_idusuarios = '$idUsuarioLogado' ORDER BY nomePost ASC";
+$exe1 = mysqli_query($con, $sql1);
 
-                $idUsuarioLogado = $_SESSION['id'];
-                $con = mysqli_connect('localhost', 'root', '', 'indieconnekt');
+if (mysqli_num_rows($exe1) > 0) {
+    echo "<table style='background-color: #2e2e4e; padding: 20px; border-radius: 10px; width: 800px;'>";
 
-                $sql = "SELECT * FROM posts WHERE id_idusuarios = '$idUsuarioLogado' ORDER BY nomePost ASC";
-                $exe = mysqli_query($con, $sql);
+    while ($res1 = mysqli_fetch_array($exe1)) {
+        $idPost = htmlspecialchars($res1['idPost']);
+        $nomePost = htmlspecialchars($res1['nomePost']);
+        $comentario = htmlspecialchars($res1['comentario']);
+        $foto = htmlspecialchars($res1['foto']);
 
-                if (mysqli_num_rows($exe) > 0) {
-                    echo "<table style='background-color: #2e2e4e; padding: 20px; border-radius: 10px; width: 800px;'>";
+        echo "<tr>
+                <th style='display: flex; justify-content: space-between; align-items: center; padding: 10px; background-color: #111; border-radius: 5px;'>$nomePost</th>
+            </tr>
+            <tr>
+                <td style='padding: 10px;'><img src='images/$foto' style='width: 400px; height: 250px; object-fit: cover; border-radius: 7px;'></td>
+                <td style='padding: 10px; font-size: 20px; font-weight: bold; max-width: 400px; border-radius: 7px; word-wrap: break-word;'>$comentario</td>
+            </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Nenhum post encontrado.";
+}
+mysqli_close($con);
+?>
 
-                    while ($res = mysqli_fetch_array($exe)) {
-                        $idPost = htmlspecialchars($res['idPost']);
-                        $nomePost = htmlspecialchars($res['nomePost']);
-                        $comentario = htmlspecialchars($res['comentario']);
-                        $foto = htmlspecialchars($res['foto']);
-
-                        echo "<tr>
-                                <th style='display: flex; justify-content: space-between; align-items: center; padding: 10px; background-color: #111; border-radius: 5px;'>$nomePost</th>
-                            </tr>
-                            <tr>
-                                <td style='padding: 10px;'><img src='images/$foto' style='width: 400px; height: 250px; object-fit: cover; border-radius: 7px;'></td>
-                                <td style='padding: 10px; font-size: 20px; font-weight: bold; max-width: 400px; border-radius: 7px; word-wrap: break-word;'>$comentario</td>
-                            </tr>";
-                    }
-                    echo "</table>";
-                } else {
-                    echo "Nenhum post encontrado.";
-                }
-
-                mysqli_close($con);
-                ?>
             </div>
         </div>
     </div>
