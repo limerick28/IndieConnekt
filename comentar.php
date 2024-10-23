3680ce6c52bb8bedc1204a0bbe92291b9f8555fd
rafaelfag4 <rafael.eugeniof@gmail.com>
2024-10-23 12:27:54 -0300

    Update comentar.php

--- comentar.php
+++ comentar.php
@@ -6,14 +6,15 @@ $nomeArq = basename($_FILES['fotocomentario']['name']);
 $uploadfile = $uploaddir.$nomeArq;
 $conexao = mysqli_connect
 ('localhost','root','','indieconnekt');
-$sql = "INSERT into comentarios (texto, fotocomentario, id_idusuarios)
-values ('$texto', '$nomeArq', $id)";
+$sql = "INSERT into comentarios (texto, fotocomentario, id_idusuarios, perfil_email)
+values ('$texto', '$nomeArq', $id, '$email')";
 echo $sql;
 $executar = mysqli_query($conexao, $sql);
 
 if (move_uploaded_file($_FILES['fotocomentario']['tmp_name'], $uploadfile)) {
 	if($executar){
 		echo "cadastrado";
+		header('location:perfil.php');
 	}
 	else{
 		echo "erro ao cadastrar";
