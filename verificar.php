6cf04a0a678076a3179281fa4493a35a83110863
rafaelfag4 <rafael.eugeniof@gmail.com>
2024-11-15 14:09:11 -0300

    Update verificar.php

--- verificar.php
+++ verificar.php
@@ -1,20 +1,17 @@
 <?php
 session_start();
-$fotouser = $_POST['fotouser'];
 $senha = $_POST['senha'];
 $login = $_POST['email'];
-$username = $_POST['username'];
 $conexao = mysqli_connect('localhost','root','','indieconnekt');
 $sql = "SELECT * FROM usuarios WHERE email like '$login'
 and senha like '$senha'";
 $executar = mysqli_query($conexao, $sql);
 $res = mysqli_fetch_array($executar);
 if($res['email'] != NULL){
-   $_SESSION['fotouser'] = $res['fotouser'];
-   $_SESSION['username'] = $res['username'];
+   
    $_SESSION['email'] = $res['email'];
    $_SESSION['id'] = $res['idusuarios'];
-   header("Location: dashboardlog.php");
+   header("Location: pagina.php");
 }
 else{
    echo "Login e/ou senha incorretos";
